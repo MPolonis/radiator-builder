5551a2c197d0bf53ac30027b0ec369c8a2c13c31
Marcin Polonis <polo@desmart.com>
2025-03-31 15:55:35 +0200

    feat: improve styles for product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -8,11 +8,13 @@ const ProductCard = ({ product }: Props) => {
   console.log(product)
   return (
     <div className="bg-white shadow-md rounded flex flex-col justify-center items-center h-[300px]">
-      <img
-        src={product.image}
-        alt={product.name}
-        className="max-w-44 max-h-44"
-      />
+      <div className="relative w-full h-[150px] flex items-center justify-center">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-auto h-auto max-w-full max-h-[120px] object-contain p-2"
+        />
+      </div>
       <div className="flex flex-col justify-start items-start p-4">
         <h3 className="font-semibold my-2 ">{product.name}</h3>
         <p className="text-sm">Length: {product.length}mm</p>
